9d42f8c729bb057e54a50013f8d48ed197a6b6c1
Bohdaniy <kukharchuk.bogdan0@gmail.com>
2025-11-26 15:40:29 +0200

    Add lab 6 and report

---
+++ WebApplicationData/Migrations/20251126121252_AddBooksTable.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplicationData.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBooksTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorId",
+                table: "AppResources",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AuthorEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Discount = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppResources_AuthorId",
+                table: "AppResources",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppResources_AspNetUsers_AuthorId",
+                table: "AppResources",
+                column: "AuthorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppResources_AspNetUsers_AuthorId",
+                table: "AppResources");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AppResources_AuthorId",
+                table: "AppResources");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorId",
+                table: "AppResources",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
